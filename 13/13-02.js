84ab80dcce780a10e821283e7ed917d39ea776ad
Boyoung Lee <osoon9295@gmail.com>
2024-11-08 11:08:38 +0900

    13강 정리

---
+++ 13/13-02.js
@@ -0,0 +1,21 @@
+var var1 = 1;
+
+if (true) {
+  var var2 = 2;
+  if (true) {
+    var var3 = 3;
+  }
+}
+
+function foo() {
+  var var4 = 4;
+  function bar() {
+    var var5 = 5;
+  }
+}
+
+console.log(var1); // 1
+console.log(var2); // 2
+console.log(var3); // 3
+console.log(var4); // ReferenceError: var4 is not defined
+console.log(var5); // ReferenceError: var5 is not defined
