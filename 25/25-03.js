28397c38d0ffb38ecaad40cc880f602c69082b0c
Boyoung Lee <osoon9295@gmail.com>
2024-11-17 12:37:31 +0900

    25강 정리

---
+++ 25/25-03.js
@@ -0,0 +1,8 @@
+// 클래스 선언문
+class Person {}
+
+// 익명 클래스 표현식
+const Person = class {};
+
+// 기명 클래스 표현식
+const Person = class MyClass {};
---
+++ 25/25-05.js
@@ -0,0 +1,4 @@
+// 클래스 선언문
+class person {}
+
+console.log(typeof Person); // function
