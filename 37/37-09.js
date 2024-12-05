e98d34177d7d7c757984b69593d39be0380235be
Boyoung Lee <osoon9295@gmail.com>
2024-12-05 13:35:02 +0900

    37강 정리

---
+++ 37/37-09.js
@@ -0,0 +1,12 @@
+const set = new Set();
+
+console.log(NaN === NaN);
+console.log(0 === -0);
+
+// NaN과 NaN을 같다고 평가하여 중복 추가를 허용하지 않는다.
+set.add(NaN).add(NaN);
+console.log(set);
+
+// +0과 -0을 같다고 평가하여 중복 추가를 허용하지 않는다.
+set.add(0).add(-0);
+console.log(set);
---
+++ 37/37-35.js
@@ -0,0 +1,14 @@
+const map = new Map();
+
+const lee = { name: "Lee" };
+const kim = { name: "Kim" };
+
+// 객체도 키로 사용할 수 있다.
+map.set(lee, "developer");
+map.set(kim, "designer");
+
+console.log(map);
+// Map(2) {
+//     { name: 'Lee' } => 'developer',
+//     { name: 'Kim' } => 'designer'
+//  }
